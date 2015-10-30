09a507cfdef358c0c8c45e8305f50c1a39f1f6fd
xxjcaxx <xxjcaxx@yahoo.es>
2015-10-30 13:42:11 +0100

    maze completado

--- rogue.py
+++ rogue.py
@@ -46,12 +46,13 @@ class pantalla(object):
           if self.mundo[i][j]=='#':
             self.actual = [i,j]
             return 0
-     for i in range(2,self.t-2,2):
-      for j in range(1,self.t-1,2):
-          self.mundo[i][j]='k'
-          if self.mundo[i][j]=='#':
-            self.actual = [i,j]
-            return 0
+    # for i in range(2,self.t-2,2):
+     # for j in range(1,self.t-1,2):
+      #    self.mundo[i][j]='k'
+       #   if self.mundo[i][j]=='#':
+        #    self.actual = [i,j]
+         #   return 0
+     self.actual = 'fin'
  
  def grow_maze(self):
      x = self.actual[0]
@@ -64,7 +65,7 @@ class pantalla(object):
      if randint(0,50)<20:
          d = randint(0,4)
      for i in range(0,4):
-         print self.direcciones[(d+i)%4]
+ #        print self.direcciones[(d+i)%4]
          if x+self.direcciones[(d+i)%4][0]>0 and x+self.direcciones[(d+i)%4][0]<self.t-1 and y+self.direcciones[(d+i)%4][1]>0 and y+self.direcciones[(d+i)%4][1]< self.t-1:
           if cavado == 0 and self.mundo[x+self.direcciones[(d+i)%4][0]*2][y+self.direcciones[(d+i)%4][1]*2]=='#':
             self.mundo[x+self.direcciones[(d+i)%4][0]][y+self.direcciones[(d+i)%4][1]]='1'
@@ -72,7 +73,15 @@ class pantalla(object):
             self.actual = [x+self.direcciones[(d+i)%4][0]*2,y+self.direcciones[(d+i)%4][1]*2]
             cavado = 1
      #print self.celdas
+     if cavado == 0:
+      self.celdas.pop()
+      if len(self.celdas) > 0:
 
+          self.actual = self.celdas.pop()
+      else:
+          self.start_maze()
+#          print 'start'
+#     print str(self.actual)+" "+str(len(self.celdas))
 
  def print_pantalla(self):
   for i in self.mundo:
@@ -82,16 +91,16 @@ class pantalla(object):
 
 
 print 'Empezamos'
-p = pantalla(49,2)
+p = pantalla(49,30)
 print(chr(27) + "[2J")
 print(chr(27)+"[s"+chr(27)+"[0;0H")
 p.print_pantalla()
 n = 0
 p.start_maze()
-while n < 1000:
+while n < 1000 and p.actual != 'fin':
     n = n+1
     p.grow_maze()
-    time.sleep(0.1)
-    print(chr(27)+"[s"+chr(27)+"[0;0H")
-    p.print_pantalla()
+   # time.sleep(0.1)
+print(chr(27)+"[s"+chr(27)+"[0;0H")
+p.print_pantalla()
 
