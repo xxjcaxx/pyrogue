6dbc3a09a5733a41c0897b8a68ff4117ffad4bc2
xxjcaxx <xxjcaxx@yahoo.es>
2015-10-31 22:01:10 +0100

    casi genera el maze

--- rogue.py
+++ rogue.py
@@ -2,12 +2,16 @@
 from random import randint
 import sys
 import time
+import threading
 
 class pantalla(object):
  
  celdas = []
  direcciones = [(1,0),(-1,0),(0,1),(0,-1)]
  d_actual = 0
+ puerta = []
+
+ puntas = []
 
  def __init__(self,t,salas):
   self.t = t
@@ -46,12 +50,6 @@ class pantalla(object):
           if self.mundo[i][j]=='#':
             self.actual = [i,j]
             return 0
-    # for i in range(2,self.t-2,2):
-     # for j in range(1,self.t-1,2):
-      #    self.mundo[i][j]='k'
-       #   if self.mundo[i][j]=='#':
-        #    self.actual = [i,j]
-         #   return 0
      self.actual = 'fin'
  
  def grow_maze(self):
@@ -81,26 +79,118 @@ class pantalla(object):
       else:
           self.start_maze()
 #          print 'start'
-#     print str(self.actual)+" "+str(len(self.celdas))
+     print '                 '
+     print str(self.actual)+" "+str(len(self.celdas))+"                      "
 
  def print_pantalla(self):
   for i in self.mundo:
    for j in i:
-    sys.stdout.write(j+' '),
+    if j == '1':
+     sys.stdout.write(chr(27)+"[34m")
+    sys.stdout.write(j+' ')
+    sys.stdout.write(chr(27)+"[0m")
    print ' '
 
+ def espunta(self,i,j):
+            n = 0
+            if self.mundo[i-1][j]=='#':
+               n = n+1
+            if self.mundo[i+1][j]=='#':
+               n = n+1
+            if self.mundo[i][j-1]=='#':
+               n = n+1
+            if self.mundo[i][j+1]=='#':
+               n = n+1
+            return n
+
+ 
+ def detectar_puntas(self):     
+     for i in range(1,self.t-1,2): # las y
+      for j in range(1,self.t-1,2): # las x
+          if self.mundo[i][j]=='1':
+            n = self.espunta(i,j)
+            if n == 3:
+               self.puntas.append([i,j])
+
+ def borrar_punta(self,punta):
+         self.mundo[punta[0]][punta[1]]='#'
+         i = punta[0]
+         j = punta[1]
+         sig = 0
+         if self.mundo[i-1][j]=='1':
+            sig = [i-1,j]
+         if self.mundo[i+1][j]=='1':
+            sig = [i+1,j] 
+         if self.mundo[i][j-1]=='1':
+            sig = [i,j-1]
+         if self.mundo[i][j+1]=='1':
+            sig = [i,j+1]
+         if sig != 0:
+          n = self.espunta(sig[0],sig[1])
+          if n == 3:
+            self.borrar_punta(sig) 
+ 
+ def detectar_puertas(self):
+     n = 0
+     self.puerta = []
+     for i in range(2,self.t-2,1): # las y
+      for j in range(2,self.t-2,1): # las x
+          #self.mundo[i][j]='k'
+          if self.mundo[i][j]=='#' or self.mundo[i][j]=='x':
+             unos = 0
+             puntos = 0
+             for d in self.direcciones:
+                 if self.mundo[i+d[0]][j+d[1]] == '1':
+                    unos = unos + 1
+                 if self.mundo[i+d[0]][j+d[1]] == '.':
+                    puntos = puntos + 1
+             if (unos == 1 and puntos == 1) or puntos == 2 :
+                 self.mundo[i][j] = 'x'
+                 self.puerta.append([i,j])
+                 n = n + 1 
+             else: 
+                 self.mundo[i][j] = '#'
+     return n
+
+ def pintar_unos(self,x,y):
+        self.mundo[x][y]='1'
+        for i in range(x-1,x+1):
+         for j in range(y-1,y+1):
+          if self.mundo[i][j] == '.':
+             self.pintar_unos(i,j)
+
+ def resolver_puertas(self):
+     while self.detectar_puertas() > 0:
+        self.puerta = self.puerta[randint(0,len(self.puerta)-1)]
+        x = self.puerta[0]
+        y = self.puerta[1]
+        self.pintar_unos(x,y) 
 
 print 'Empezamos'
-p = pantalla(49,30)
+p = pantalla(49,20)
 print(chr(27) + "[2J")
 print(chr(27)+"[s"+chr(27)+"[0;0H")
 p.print_pantalla()
 n = 0
 p.start_maze()
-while n < 1000 and p.actual != 'fin':
+while p.actual != 'fin':
     n = n+1
     p.grow_maze()
-   # time.sleep(0.1)
+#    time.sleep(0.05)
 print(chr(27)+"[s"+chr(27)+"[0;0H")
 p.print_pantalla()
 
+time.sleep(5)
+
+p.resolver_puertas()   # no pinta los extremos y detecta dos puertas a veces          
+ 
+p.detectar_puntas()
+while len(p.puntas)>0:
+ i = p.puntas.pop()
+ t = threading.Thread(target=p.borrar_punta(i))
+ t.start()
+
+#print p.puntas
+
+print(chr(27)+"[s"+chr(27)+"[0;0H")
+p.print_pantalla()
