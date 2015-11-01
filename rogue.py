67925a5261ecce6c4dd9b044391a15842640ff7d
xxjcaxx <xxjcaxx@yahoo.es>
2015-11-01 13:35:37 +0100

    maze acabado

--- rogue.py
+++ rogue.py
@@ -10,13 +10,14 @@ class pantalla(object):
  direcciones = [(1,0),(-1,0),(0,1),(0,-1)]
  d_actual = 0
  puerta = []
-
  puntas = []
+ m = 0
 
  def __init__(self,t,salas):
   self.t = t
-  self.mundo = [['#' for x in range(t)] for x in range(t)]
+  self.mundo = [[['#',0] for x in range(t)] for x in range(t)]
   exitos = 0
+  self.m = salas
   for s in range(2,salas+2):
    cabe = 0
    intentos = 0
@@ -33,7 +34,7 @@ class pantalla(object):
      y=y-1
     for i in range(x-1,x+ax+1):
      for j in range(y-1,y+ay+1):
-      if self.mundo[i][j] != '#':
+      if self.mundo[i][j][0] != '#':
        chocan = 1
     if chocan == 0:
      cabe = 1
@@ -41,21 +42,22 @@ class pantalla(object):
      exitos = exitos + 1
      for i in range(x,x+ax):
       for j in range(y,y+ay):
-       self.mundo[i][j]=str(s)
+       self.mundo[i][j]=['.',s]
   #print exitos
  def start_maze(self):
      for i in range(1,self.t-1,2): # las y
       for j in range(1,self.t-1,2): # las x
           #self.mundo[i][j]='k'
-          if self.mundo[i][j]=='#':
+          if self.mundo[i][j][0]=='#':
             self.actual = [i,j]
+            self.m = self.m + 1
             return 0
      self.actual = 'fin'
  
  def grow_maze(self):
      x = self.actual[0]
      y = self.actual[1]
-     self.mundo[x][y]='1'
+     self.mundo[x][y]=['1',self.m]
      self.celdas.append(self.actual)
      # el tema de la diección y las dirección actual
      cavado = 0 # si ya he cavado en alguna direccion
@@ -65,8 +67,8 @@ class pantalla(object):
      for i in range(0,4):
  #        print self.direcciones[(d+i)%4]
          if x+self.direcciones[(d+i)%4][0]>0 and x+self.direcciones[(d+i)%4][0]<self.t-1 and y+self.direcciones[(d+i)%4][1]>0 and y+self.direcciones[(d+i)%4][1]< self.t-1:
-          if cavado == 0 and self.mundo[x+self.direcciones[(d+i)%4][0]*2][y+self.direcciones[(d+i)%4][1]*2]=='#':
-            self.mundo[x+self.direcciones[(d+i)%4][0]][y+self.direcciones[(d+i)%4][1]]='1'
+          if cavado == 0 and self.mundo[x+self.direcciones[(d+i)%4][0]*2][y+self.direcciones[(d+i)%4][1]*2][0]=='#':
+            self.mundo[x+self.direcciones[(d+i)%4][0]][y+self.direcciones[(d+i)%4][1]]=['1',self.m]
             self.d_actual = (d+i)%4
             self.actual = [x+self.direcciones[(d+i)%4][0]*2,y+self.direcciones[(d+i)%4][1]*2]
             cavado = 1
@@ -85,21 +87,43 @@ class pantalla(object):
  def print_pantalla(self):
   for i in self.mundo:
    for j in i:
-    if j == '1':
+    if j[0] == '1':
+     sys.stdout.write(chr(27)+"["+str(30+(j[1]%8))+"m")
+    if j[0] == '.':
+     sys.stdout.write(chr(27)+"["+str(j[1])+"m")
+    sys.stdout.write(j[0]+' ')
+    sys.stdout.write(chr(27)+"[0m")
+   print ' '
+ def print_pantalla_final(self):
+  for i in self.mundo:
+   for j in i:
+    if j[0] == '1':
+     sys.stdout.write(chr(27)+"[37m")
+    if j[0] == '.':
+     sys.stdout.write(chr(27)+"["+str(j[1])+"m")
+    sys.stdout.write(j[0]+' ')
+    sys.stdout.write(chr(27)+"[0m")
+   print ' '
+ def print_pantalla_debug(self):
+  for i in self.mundo:
+   for j in i:
+    if j[0] == '1':
      sys.stdout.write(chr(27)+"[34m")
-    sys.stdout.write(j+' ')
+    if j[0] == '.':
+     sys.stdout.write(chr(27)+"["+str(j[1])+"m")
+    sys.stdout.write(str(j[1])+' ')
     sys.stdout.write(chr(27)+"[0m")
    print ' '
 
  def espunta(self,i,j):
             n = 0
-            if self.mundo[i-1][j]=='#':
+            if self.mundo[i-1][j][0]=='#':
                n = n+1
-            if self.mundo[i+1][j]=='#':
+            if self.mundo[i+1][j][0]=='#':
                n = n+1
-            if self.mundo[i][j-1]=='#':
+            if self.mundo[i][j-1][0]=='#':
                n = n+1
-            if self.mundo[i][j+1]=='#':
+            if self.mundo[i][j+1][0]=='#':
                n = n+1
             return n
 
@@ -107,23 +131,23 @@ class pantalla(object):
  def detectar_puntas(self):     
      for i in range(1,self.t-1,2): # las y
       for j in range(1,self.t-1,2): # las x
-          if self.mundo[i][j]=='1':
+          if self.mundo[i][j][0]=='1':
             n = self.espunta(i,j)
             if n == 3:
                self.puntas.append([i,j])
 
  def borrar_punta(self,punta):
-         self.mundo[punta[0]][punta[1]]='#'
+         self.mundo[punta[0]][punta[1]]=['#',0]
          i = punta[0]
          j = punta[1]
          sig = 0
-         if self.mundo[i-1][j]=='1':
+         if self.mundo[i-1][j][0]=='1':
             sig = [i-1,j]
-         if self.mundo[i+1][j]=='1':
+         if self.mundo[i+1][j][0]=='1':
             sig = [i+1,j] 
-         if self.mundo[i][j-1]=='1':
+         if self.mundo[i][j-1][0]=='1':
             sig = [i,j-1]
-         if self.mundo[i][j+1]=='1':
+         if self.mundo[i][j+1][0]=='1':
             sig = [i,j+1]
          if sig != 0:
           n = self.espunta(sig[0],sig[1])
@@ -136,41 +160,49 @@ class pantalla(object):
      for i in range(2,self.t-2,1): # las y
       for j in range(2,self.t-2,1): # las x
           #self.mundo[i][j]='k'
-          if self.mundo[i][j]=='#' or self.mundo[i][j]=='x':
-             unos = 0
-             puntos = 0
-             for d in self.direcciones:
-                 if self.mundo[i+d[0]][j+d[1]] == '1':
-                    unos = unos + 1
-                 if self.mundo[i+d[0]][j+d[1]] not in  ['1','#','x']:
-                    puntos = puntos + 1
-             if (unos == 1 and puntos == 1) or puntos == 2 :
-                 self.mundo[i][j] = 'x'
-                 self.puerta.append([i,j])
-                 n = n + 1 
-             else: 
-                 self.mundo[i][j] = '#'
+          if self.mundo[i][j][0]=='#':
+              regiones = 0
+              num = 0
+              for d in self.direcciones:
+                  if self.mundo[i+d[0]][j+d[1]][1] > 0: # es una region
+                     if regiones == 0:
+                        num = self.mundo[i+d[0]][j+d[1]][1]
+                        regiones = regiones + 1
+                     if regiones > 0:
+                        if num != self.mundo[i+d[0]][j+d[1]][1]:
+                          # self.mundo[i][j] = ['x',0]
+                           self.puerta.append([i,j,num])
+                           n = n +1
+                        else:
+                           self.mundo[i][j] = ['#',0]
      return n
 
- def pintar_unos(self,x,y):
-        self.mundo[x][y]='1'
+ def pintar_unos(self,x,y,n):
+        self.mundo[x][y]=['1',n]
         for i in range(x-1,x+2):
          for j in range(y-1,y+2):
-          if self.mundo[i][j] not in ['1','#','x']:
-             self.pintar_unos(i,j)
+          if self.mundo[i][j][0] != '#' and self.mundo[i][j][1] != n:
+             self.pintar_unos(i,j,n)
 
  def resolver_puertas(self):
+     primera_puerta = 0
      while self.detectar_puertas() > 0:
         p = self.puerta[randint(0,len(self.puerta)-1)]
         x = p[0]
         y = p[1]
-        self.pintar_unos(x,y) 
-        time.sleep(0.5)
+        n = p[2]
+
+        if primera_puerta == 0:
+           primera_puerta = [x,y]
+
+        self.pintar_unos(x,y,n) 
+        #time.sleep(0.1)
         print(chr(27)+"[s"+chr(27)+"[0;0H")
         self.print_pantalla()
+     self.pintar_unos(primera_puerta[0],primera_puerta[1],1)
 
 print 'Empezamos'
-p = pantalla(51,50)
+p = pantalla(51,5)
 print(chr(27) + "[2J")
 print(chr(27)+"[s"+chr(27)+"[0;0H")
 p.print_pantalla()
@@ -181,11 +213,11 @@ while p.actual != 'fin':
     p.grow_maze()
 #    time.sleep(0.05)
 print(chr(27)+"[s"+chr(27)+"[0;0H")
-p.print_pantalla()
+#p.print_pantalla_debug()
 
-time.sleep(0.5)
+#time.sleep(0.5)
 
-p.resolver_puertas()   # no pinta los extremos y detecta dos puertas a veces          
+p.resolver_puertas()           
  
 p.detectar_puntas()
 while len(p.puntas)>0:
@@ -196,4 +228,4 @@ while len(p.puntas)>0:
 #print p.puntas
 
 print(chr(27)+"[s"+chr(27)+"[0;0H")
-p.print_pantalla()
+p.print_pantalla_final()
